cf363a767c7bfaf7de85fa76b6acfaa253da6afb
brunolimo125 <brunolimo125@gmail.com>
2024-09-03 23:07:36 -0300

    adiciona novos templates

--- aula2/jinja/processa_template.py
+++ aula2/jinja/processa_template.py
@@ -2,17 +2,17 @@ import jinja2
 import yaml
 import os
 
-def renderiza_template():
-    with open('redshift.yml.j2','r') as f:
-        readshift_yml = f.read()
 
-    with open('config.yaml','r') as f:
+def renderiza_template():
+    with open('redshift.yaml.j2', 'r') as f:
+        redshift_yaml = f.read()
+
+    with open('config.yaml', 'r') as f:
         config = yaml.safe_load(f)
 
-    redshift_template = jinja2.Template(redshift_yml)
-    rendshift_rendered = redshift_template.render({**config,**os.environ})
 
-    with open('redshift.yml', r) as f:
-        f.write(rendshift_rendered)
+    redshift_template = jinja2.Template(redshift_yaml)
+    redshift_rendered = redshift_template.render({**config, **os.environ})
 
-renderiza_template()
+    with open('redshift.yaml', 'w') as f:
+        f.write(redshift_rendered)
